552b682ebac9a40363ec74124e07a336dfa3b91a
Clare S <Clare.Stock@gmail.com>
2024-06-12 17:25:24 -0400

    first commit

---
+++ angularlab/src/app/app.component.ts
@@ -0,0 +1,82 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MenuItem } from './models/menu-item';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title:string = 'Grand Chirpus';
+
+  menuitems:MenuItem[] = [
+
+    {
+      name: "Cheese Pizza",
+      category: "dinner",
+      price: 10.00,
+      vegetarian: true,
+      soldOut: false
+    },
+    {
+      name: "Pepperoni Pizza",
+      category: "dinner",
+      price: 10.00,
+      vegetarian: false,
+      soldOut: true
+    },
+    {
+      name: "Margherita Pizza",
+      category: "dinner",
+      price: 10.00,
+      vegetarian: true,
+      soldOut: false
+    },
+    {
+      name: "Chicken Tenders",
+      category: "dinner",
+      price: 8.50,
+      vegetarian: false,
+      soldOut: false
+    },
+    {
+      name: "Garden Stuffed Potato",
+      category: "dinner",
+      price: 8.50,
+      vegetarian: true,
+      soldOut: true
+    },
+    {
+      name: "Chili Cheese Fries",
+      category: "sides",
+      price: 6.75,
+      vegetarian: false,
+      soldOut: false
+    },
+    {
+      name: "Classic Seasoned Fries",
+      category: "sides",
+      price: 5.25,
+      vegetarian: true,
+      soldOut: false
+    },
+    {
+      name: "Brownie Ice Cream Sundae",
+      category: "dessert",
+      price: 5.00,
+      vegetarian: false,
+      soldOut: false
+    },
+    {
+      name: "Cherry Pie",
+      category: "dessert",
+      price: 5.00,
+      vegetarian: true,
+      soldOut: true
+    }
+
+  ]
+}
